5f83e8303792d4aa104403682727a101a6fe0d17
Ryan <xxiryanixx@hotmail.com>
2020-06-29 18:50:38 +1000

    fixed up product item widget

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-
 class ProductItem extends StatelessWidget {
-
   final String id;
   final String title;
   final String imageUrl;
@@ -11,6 +9,28 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(imageUrl),);
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.shopping_cart,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
