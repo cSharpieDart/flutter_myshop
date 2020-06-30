56cc5c598fd0d25bcbca5c435cf756e2950fbf2b
Ryan <xxiryanixx@hotmail.com>
2020-06-30 20:00:39 +1000

    widget changes

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  final String title;
 
-  ProductDetailScreen(this.title);
+  static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
+
+    final productId = ModalRoute.of(context).settings.arguments as String; //gets the id from arguments of rotue
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          title,
-        ),
+          "",
+        ), 
       ),
     );
   }
