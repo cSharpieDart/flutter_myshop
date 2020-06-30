48005b72a77b77a25dba1ab64fcdacca2bc0de2c
Ryan <xxiryanixx@hotmail.com>
2020-06-30 21:31:44 +1000

    used provider to get title

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments
+        as String; //gets the id from arguments of rotue
 
-    final productId = ModalRoute.of(context).settings.arguments as String; //gets the id from arguments of rotue
+    final loadedProduct = Provider.of<Products>(context)
+        .items
+        .firstWhere((prod) => prod.id == productId);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "",
-        ), 
+          loadedProduct.title,
+        ),
       ),
     );
   }
